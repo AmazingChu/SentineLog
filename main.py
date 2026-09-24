49c399ecceacbbbc495a2a368f1b325c67395855
AmazingChu <k193992378@gmail.com>
2026-09-24 14:30:58 +0930

    docs: add code comments and docstrings.

--- main.py
+++ main.py
@@ -6,11 +6,18 @@ LOG_FILE = "logs/sample_access.log"
 OUTPUT_FILE = "output/suspicious_ips.csv"
 
 def main():
-
+    """
+    Work flow:
+    1. Parse the log file
+    2. Detect suspicious IP and activity
+    3. Display in terminal
+    4. Export the result to CSV
+    """
     logs = parse_log(LOG_FILE)
 
     results = detect_suspicious_ips(logs)
 
+    #Display in terminal
     generate_report(results)
 
     export_to_csv(results, OUTPUT_FILE)
